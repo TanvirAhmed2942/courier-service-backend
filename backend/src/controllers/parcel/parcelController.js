b50f6682fe0487498595e87818af8a858e840fef
Tanvir Ahmed Swapnil <tanvirahmed.3264@gmail.com>
2025-12-08 23:28:01 +0600

    parcel

--- backend/src/controllers/parcel/parcelController.js
+++ backend/src/controllers/parcel/parcelController.js
@@ -1,4 +1,5 @@
 import Parcel from "../../models/parcel/Parcel.js";
+
 export const createParcel = async (req, res) => {
   console.log("Creating parcel: ", req.body);
   try {
@@ -49,5 +50,139 @@ export const createParcel = async (req, res) => {
       error: error.message,
     });
     console.log("Error creating parcel (createParcel): ", error);
+  }
+};
+
+export const getParcels = async (req, res) => {
+  console.log("Getting parcels: ", req.body);
+  try {
+    const parcels = await Parcel.find({ sender: req.user._id });
+    res.status(200).json({
+      status: "success",
+      message: "Parcels fetched successfully",
+      data: parcels,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Error fetching parcels",
+      error: error.message,
+    });
+    console.log("Error fetching parcels (getParcels): ", error);
+  }
+};
+
+export const getParcel = async (req, res) => {
+  console.log("Getting parcel: ", req.params.id);
+  try {
+    const parcel = await Parcel.findById(req.params.id);
+    if (!parcel) {
+      return res.status(404).json({
+        status: "error",
+        message: "Parcel not found",
+      });
+    }
+    if (!req.user._id.equals(parcel.sender)) {
+      return res.status(401).json({
+        status: "error",
+        message: "Unauthorized: You are not the sender of this parcel",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Parcel fetched successfully",
+      data: parcel,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Error fetching parcel",
+      error: error.message,
+    });
+    console.log("Error fetching parcel (getParcel): ", error);
+  }
+};
+
+export const updateParcel = async (req, res) => {
+  console.log("Updating parcel: ", req.params.id);
+
+  try {
+    const parcel = await Parcel.findById(req.params.id);
+    if (!parcel) {
+      return res.status(404).json({
+        status: "error",
+        message: "Parcel not found",
+      });
+    }
+    if (!req.user.id.toString() === parcel.sender.toString()) {
+      return res.status(401).json({
+        status: "error",
+        message: "Unauthorized: You are not the sender of this parcel",
+      });
+    }
+    const updatedParcel = await Parcel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({
+      status: "success",
+      message: "Parcel updated successfully",
+      data: updatedParcel,
+    });
+    console.log("Parcel updated successfully (updateParcel): ", updatedParcel);
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Error updating parcel",
+      error: error.message,
+    });
+    console.log("Error updating parcel (updateParcel): ", error);
+  }
+};
+
+export const cancelParcel = async (req, res) => {
+  console.log("Cancelling parcel: ", req.params.id);
+  try {
+    const parcel = await Parcel.findById(req.params.id);
+    if (!parcel) {
+      return res.status(404).json({
+        status: "error",
+        message: "Parcel not found",
+      });
+    }
+    if (!req.user.id.toString() === parcel.sender.toString()) {
+      return res.status(401).json({
+        status: "error",
+        message: "Unauthorized: You are not the sender of this parcel",
+      });
+    }
+    const cancelledParcel = await Parcel.findByIdAndUpdate(
+      req.params.id,
+      {
+        deliveryStatus: "cancelled",
+      },
+      {
+        new: true,
+      }
+    );
+    console.log(
+      "Parcel cancelled successfully (cancelParcel): ",
+      cancelledParcel
+    );
+    res.status(200).json({
+      status: "success",
+      message: "Parcel cancelled successfully",
+      data: cancelledParcel,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Error cancelling parcel",
+      error: error.message,
+    });
+    console.log("Error cancelling parcel (cancelParcel): ", error);
   }
 };
--- backend/src/routes/parcel/parcelRoutes.js
+++ backend/src/routes/parcel/parcelRoutes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createParcel } from "../../controllers/parcel/parcelController.js";
+import {
+  createParcel,
+  getParcels,
+  getParcel,
+  updateParcel,
+  cancelParcel,
+} from "../../controllers/parcel/parcelController.js";
 import {
   authMiddleware,
   isUser,
@@ -11,6 +17,10 @@ const router = express.Router();
 
 //user
 router.post("/create-parcel", authMiddleware, isUser, createParcel);
+router.get("/get-parcels", authMiddleware, isUser, getParcels);
+router.get("/get-parcel/:id", authMiddleware, isUser, getParcel);
+router.put("/update-parcel/:id", authMiddleware, isUser, updateParcel);
+router.patch("/cancel-parcel/:id", authMiddleware, isUser, cancelParcel);
 
 //admin
 // router.post("/assign-agent", authMiddleware, isAdmin, assignAgent);
